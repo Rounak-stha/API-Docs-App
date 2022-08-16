33a2133edd0114db178daa565172b9390b160eda
Rouak-stha <rounakshrestha1153@gmail.com>
2022-08-16 16:50:32 +0545

    Added name to Error page

--- pages/error/500.js
+++ pages/error/500.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 
-export default function Eror500() {
+export default function E500() {
     useEffect(() => {
         if (!document.referrer) window.location.href = '/'
     })
